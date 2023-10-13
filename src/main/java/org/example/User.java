bb17cce8308ee9b6d40f830b2ef63b15ae163b15
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-13 12:12:20 +0200

    Initializing the User class

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class User {
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String password;
+
+    @Getter
+    @Setter
+    private String email;
+}

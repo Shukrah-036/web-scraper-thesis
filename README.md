8ab6703a71e3a585ba28b670bea859d39c6224fa
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-13 12:11:57 +0200

    Initializing the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web-scraper-thesis
+
 For my thesis, this is a web scraper app developed using Java and Spring in the backend, and thymeleaf in the frontend.
---
+++ src/main/java/com/example/webscraperthesis/WebScraperThesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.webscraperthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperThesisApplication.class, args);
+	}
+
+}

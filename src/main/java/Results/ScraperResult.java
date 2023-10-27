24c0aee8cdcbe81049e96ddce8d9d2bd865d6dff
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-27 09:44:43 +0200

    Updating the ScraperResult class

--- src/main/java/Results/ScraperResult.java
+++ src/main/java/Results/ScraperResult.java
@@ -1,5 +1,6 @@
 package Results;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.scrapernest.Item;
@@ -7,21 +8,28 @@ import org.scrapernest.Item;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class ScraperResults {
+@Builder
+public class ScraperResult {
 
     @Getter
+    @Setter
+    private String scraperName;
+
+    @Getter
+    @Setter
     private LocalDateTime timeStamp;
 
     @Getter
+    @Setter
     private List<String> extractedData;
-
-    @Getter
-    private String scraperName;
-
-    private boolean success;
 
     @Getter
     @Setter
     private Item associatedItem;
+
+    @Getter
+    @Setter
+    private boolean success;
+
 
 }
